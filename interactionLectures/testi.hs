7658cff50fe3356d8cacacbed85962076e976409
Toni Pikkarainen <tonipikkarainen@Toni-MBP.home>
2019-10-03 19:19:58 +0300

    interactionlecture 4

--- interactionLectures/testi.hs
+++ interactionLectures/testi.hs
@@ -9,6 +9,13 @@
 g ::  [a] -> (a->a) -> [a] 
 g =  (\xs -> \ f -> map f xs)
 
-f :: [a] -> (a->a)
-f (a:xs) x = a
-f [] x = x 
+f:: [a] -> (a->a)
+f = (\xs -> \x ->  head xs) 
+--f :: [a] -> (a->a)
+--f (a:xs) x = a
+--f [] x = x 
+
+kaanna as = case as of
+    [] -> []
+    [x] -> [x]
+    (x:xs) -> (kaanna xs) ++ [x]
