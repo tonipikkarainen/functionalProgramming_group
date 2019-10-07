1bf192ea7d92a961a5806a5fdf2ba6dc9b5ea8ec
Toni Pikkarainen <tonipikkarainen@Toni-MacBook-Pro.local>
2019-10-07 14:19:19 +0300

    korjauksia IL2:een

--- interactionLectures/testi.hs
+++ interactionLectures/testi.hs
@@ -6,16 +6,36 @@
 
 --h :: a 
 
-g ::  [a] -> (a->a) -> [a] 
-g =  (\xs -> \ f -> map f xs)
+--g ::  [a] -> (a->a) -> [a] 
+--g =  (\xs -> \ f -> map f xs)
+--
+--f:: [a] -> (a->a)
+--f = (\xs -> \x ->  head xs) 
+----f :: [a] -> (a->a)
+----f (a:xs) x = a
+----f [] x = x 
+--
+--kaanna as = case as of
+--    [] -> []
+--    [x] -> [x]
+--    (x:xs) -> (kaanna xs) ++ [x]
+--
+--j :: Int -> a -> Bool
+--j x y = case y of
+--    (x:xs) -> True
+--    _ ->  False 
 
-f:: [a] -> (a->a)
-f = (\xs -> \x ->  head xs) 
---f :: [a] -> (a->a)
---f (a:xs) x = a
---f [] x = x 
+tarkista xs = case xs of
+    x:(y:(z:(t:(s:ys)))) -> True
+    otherwise -> False
 
-kaanna as = case as of
-    [] -> []
-    [x] -> [x]
-    (x:xs) -> (kaanna xs) ++ [x]
+f = case Just (Left 1) of
+    Nothing        -> "A"  
+    Just (Right 1) -> "B" 
+    Just x         -> "C" 
+    Just (Left 1)  -> "D" 
+
+data PyType = PyBool Bool
+            | PyInt Int
+            | PyDouble Double
+            | PyFun ([PyType] -> IO PyType)
