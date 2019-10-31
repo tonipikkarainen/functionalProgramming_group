52c70f22b6ab4cefe2a8630f41b303729882cd1c
ElinaSiv <elina.t.sivula@student.jyu.fi>
2019-10-31 11:25:04 +0200

    JSON-tehtävä

--- credit4/ExTyClass3.hs
+++ credit4/ExTyClass3.hs
@@ -1,5 +1,28 @@
-import Data.Text 
-class JSON a where
-  definitions
+import Data.Text
 
-toJSONString :: JSON a => a -> String
+
+class JSON a where
+  toJSONString :: a -> String
+
+--instance JSON Int where
+  --toJSONString x = show x
+
+instance JSON Integer where
+  toJSONString x = show x
+
+instance JSON Char where
+  toJSONString a = [a] -- tai show a
+
+instance JSON Text where
+  toJSONString t = unpack t
+
+instance (JSON a, JSON b) =>JSON (a,b) where
+  toJSONString (a,b) = "[" ++ toJSONString a ++ toJSONString b ++ "]"
+
+instance (JSON a) => JSON [a] where
+  toJSONString [] = []
+  toJSONString (x:xs) = "[" ++ apu (x:xs) ++"]"
+      where
+        apu [] = ""
+        apu [x] = toJSONString x
+        apu (x:xs) = toJSONString x ++ "," ++ apu xs
